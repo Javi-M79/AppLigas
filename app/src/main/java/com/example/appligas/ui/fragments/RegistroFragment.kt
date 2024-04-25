763661a7bc3f92949eb78b9a3f9ef21af797f2c2
Silx79 <javier.mira46@gmail.com>
2024-04-25 19:26:19 +0200

    Implementacion de registro mediante FIREBASE

--- app/src/main/java/com/example/appligas/ui/fragments/RegistroFragment.kt
+++ app/src/main/java/com/example/appligas/ui/fragments/RegistroFragment.kt
@@ -7,15 +7,18 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.appligas.R
 import com.example.appligas.databinding.FragmentRegistroBinding
+import com.example.appligas.model.Usuario
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistroFragment() : Fragment(), OnClickListener {
 
 
     private lateinit var binding: FragmentRegistroBinding
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -28,6 +31,8 @@ class RegistroFragment() : Fragment(), OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        /*Inicio de la instancia de Firebase*/
+        auth = FirebaseAuth.getInstance()
         binding.BtnRegistrar.setOnClickListener(this)
 
     }
@@ -47,12 +52,43 @@ class RegistroFragment() : Fragment(), OnClickListener {
 
         when (v?.id) {
             binding.BtnRegistrar.id -> {
+                if (binding.editnombreRegistro.text.isNotEmpty()
+                    && binding.editCorreoRegistro.text.isNotEmpty()
+                    && binding.editPassRegistro.text.isNotEmpty()
+                ) {
+                    //REGISTRO DE USUARIO EN FIREBASE
+                    val nombreUsuario = binding.editnombreRegistro.text.toString()
+                    val mailUsuario = binding.editCorreoRegistro.text.toString()
+                    val passwordUsuario = binding.editPassRegistro.text.toString()
+                    val usuario: Usuario = Usuario(nombreUsuario, mailUsuario, passwordUsuario)
+                    auth.createUserWithEmailAndPassword(mailUsuario, passwordUsuario)
+                        //COMPROBACION DE LA INSERCION
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Snackbar.make(
+                                    binding.root,
+                                    "Registro realizado con exito",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
+                                //Volvemos a la pantsalla anterior.
+                                findNavController().navigate(R.id.action_registroFragment_to_loginFragment)
+                            } else {
+                                Snackbar.make(
+                                    binding.root,
+                                    "No se ha podido realizar el registro. ${task.exception?.message}",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
 
-                Snackbar.make(
-                    binding.root,
-                    resources.getString(R.string.botonregistro),
-                    Snackbar.LENGTH_SHORT
-                ).show()
+                } else {
+                    Snackbar.make(
+                        binding.root,
+                        "Introduzca sus datos por favor",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+
             }
 
         }
