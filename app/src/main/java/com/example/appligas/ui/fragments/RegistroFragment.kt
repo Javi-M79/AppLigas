1b7cea8bdba0bf0dbd76b7f0f9da601b7f948a80
Silx79 <javier.mira46@gmail.com>
2024-04-27 08:26:01 +0200

    Registro e insercion en Realtime DataBaste de Firebase

--- app/src/main/java/com/example/appligas/ui/fragments/RegistroFragment.kt
+++ app/src/main/java/com/example/appligas/ui/fragments/RegistroFragment.kt
@@ -10,15 +10,19 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.appligas.R
 import com.example.appligas.databinding.FragmentRegistroBinding
+import com.example.appligas.model.Equipo
 import com.example.appligas.model.Usuario
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegistroFragment() : Fragment(), OnClickListener {
 
 
     private lateinit var binding: FragmentRegistroBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    lateinit var listaFavoritos: ArrayList<Equipo>
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -56,11 +60,11 @@ class RegistroFragment() : Fragment(), OnClickListener {
                     && binding.editCorreoRegistro.text.isNotEmpty()
                     && binding.editPassRegistro.text.isNotEmpty()
                 ) {
-                    //REGISTRO DE USUARIO EN FIREBASE
+                    //REGISTRO DE USUARIO EN FIREBASE (autenticacion)
                     val nombreUsuario = binding.editnombreRegistro.text.toString()
                     val mailUsuario = binding.editCorreoRegistro.text.toString()
                     val passwordUsuario = binding.editPassRegistro.text.toString()
-                    val usuario: Usuario = Usuario(nombreUsuario, mailUsuario, passwordUsuario)
+
                     auth.createUserWithEmailAndPassword(mailUsuario, passwordUsuario)
                         //COMPROBACION DE LA INSERCION
                         .addOnCompleteListener { task ->
@@ -70,7 +74,15 @@ class RegistroFragment() : Fragment(), OnClickListener {
                                     "Registro realizado con exito",
                                     Snackbar.LENGTH_SHORT
                                 ).show()
-                                //Volvemos a la pantsalla anterior.
+                                //Creamos una referencia del usuario para llevarlo a DB
+                                database = FirebaseDatabase.getInstance()
+                                //Crear el usuario despues de crear la instancia de la DB.
+                                val usuario: Usuario = Usuario(nombreUsuario, mailUsuario, passwordUsuario)
+                                val usuarioReferencia =
+                                    database.getReference("usuarios").child(auth.currentUser!!.uid)
+//                                //Guardamos todos los datos del usuario creado
+                                usuarioReferencia.setValue(usuario)
+                                //Volvemos a la pantalla de inicio
                                 findNavController().navigate(R.id.action_registroFragment_to_loginFragment)
                             } else {
                                 Snackbar.make(
